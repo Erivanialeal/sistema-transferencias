348d3fcff630040708441df457c85c192f3f694a
Erivania Leal <erivanial.s1507@gmail.com>
2026-09-16 20:35:58 -0300

    Primeiro commit

---
+++ src/main/java/br/com/erivania/sistema_transferencias/SistemaTransferenciasApplication.java
@@ -0,0 +1,12 @@
+package br.com.erivania.sistema_transferencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class SistemaTransferenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTransferenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erivania/sistema_transferencias/SistemaTransferenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erivania.sistema_transferencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTransferenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
