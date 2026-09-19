66bcd92d951be0735013a1c186d66fc53dc9c252
Erivania Leal <erivanial.s1507@gmail.com>
2026-09-18 22:35:24 -0300

    Implementa entidades no domain Usuario

---
+++ src/main/java/br/com/erivania/sistema_transferencias/usuario/domain/TipoUsuario.java
@@ -0,0 +1,7 @@
+package br.com.erivania.sistema_transferencias.usuario.domain;
+
+public enum TipoUsuario {
+    COMUM,LOJIISTA
+
+
+}
---
+++ src/main/java/br/com/erivania/sistema_transferencias/usuario/domain/Usuario.java
@@ -0,0 +1,33 @@
+package br.com.erivania.sistema_transferencias.usuario.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue
+    private UUID idUsuario;
+    @NotBlank
+    private String nome;
+    @NotBlank
+    @Pattern(regexp = "//d{11}")
+    private String cpf;
+    @NotBlank
+    private String email;
+    @NotBlank
+    @Size(min = 8)
+    private String senha;
+    @Enumerated(EnumType.STRING)
+    private TipoUsuario tipo;
+    @NotNull
+    private BigDecimal saldo;
+}
